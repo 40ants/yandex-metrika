686b1250b491aab0b90a25beb8f2f85c2f2ebc17
Alexander Artemenko <svetlyak.40wt@gmail.com>
2025-12-13 13:14:35 +0300

    Added token var.

--- src/vars.lisp
+++ src/vars.lisp
@@ -1,8 +1,15 @@
 (uiop:define-package #:yandex-metrika/vars
   (:use #:cl)
-  (:export #:*counter*))
+  (:export #:*counter*
+           #:*token*))
 (in-package #:yandex-metrika/vars)
 
 
 (defvar *counter* nil
   "Yandex Metrika's counter id.")
+
+
+(defvar *token* nil
+  "Yandex Metrika's OAuth token.
+
+   Use this link to get dev token https://oauth.yandex.ru/authorize?response_type=token&client_id=b7e202e6bbd548eb9f0e25dd94eaf6a4 or create your own OAuth client application.")
